77e58f084f7b50184581de818fecd8c788d73dc8
Gabriel Caliari <gabriel15caliari@hotmail.com>
2024-07-10 14:57:37 -0300

    inserindo as fontes e cores

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,10 @@
  import { Text, View} from "react-native";
  export default function Index(){
     return (
-      <View>
-        <Text>Teste</Text>
+      <View className="flex-1 items-center justify-center">
+        <Text>
+          Convide seus amigos e planeje sua próxima viagem
+        </Text>
       </View>
     )
   }
---
+++ src/components/loading.tsx
@@ -0,0 +1,5 @@
+import { ActivityIndicator } from "react-native";
+
+export function Loading () {
+  return <ActivityIndicator className="flex-1 bg-zinc-950 justify-center text-lime-300"/>;
+}
