d4867cb6be8efca9a15a75dba0a9d92de98f4090
Gabriel Caliari <gabriel15caliari@hotmail.com>
2024-07-11 10:09:28 -0300

    inserindo o input nos components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,17 @@
- import { Text, View} from "react-native";
+ import { Input } from "@/components/input";
+import { Image, Text, View} from "react-native";
  export default function Index(){
     return (
       <View className="flex-1 items-center justify-center">
-        <Text>
-          Convide seus amigos e planeje sua próxima viagem
+        <Image source={require("@/assets/logo.png")} className="h-8" resizeMode="contain"/>
+        <Text className="text-zinc-400 font-regular text-center text-lg mt-3">
+          Convide seus amigos e planeje sua{"\n"}próxima viagem
         </Text>
+        <View>
+          <Input variant="primary">
+            <Input.Field />
+          </Input>
+        </View>
       </View>
     )
   }
---
+++ src/components/input.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from "react";
+import { TextInput, TextInputProps, View } from "react-native";
+import clsx from 'clsx'
+import { colors } from "@/styles/colors";
+
+type Variants = 'primary' | 'secondary' | 'tertiary'
+
+type InputProps = {
+  children: ReactNode
+  variant?: Variants
+}
+
+function Input({children, variant = 'primary'}: InputProps) {
+  return (
+    <View className={clsx(
+    "w-full h-16 flex-row items-center gap-2",
+    {"h-14 px-4 rounded-lg border-zinc-800": variant !== "primary"},
+    {"bg-zinc-950": variant === "secondary"},
+    {"bg-zinc-900": variant === "tertiary"}
+  )}>
+      {children}
+    </View>
+  )
+}
+
+function Field({...rest}: TextInputProps) {
+  return (
+  <TextInput
+  className="flex-1 text-zinc-100 text-lg font-regular"
+  placeholderTextColor={colors.zinc[400]}
+  cursorColor={colors.zinc[100]}
+  {...rest}
+  />
+  )
+}
+
+Input.Field = Field;
+
+export {Input}
