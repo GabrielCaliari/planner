def5d2fcf5d770b5b54c804742dcf8bb76ae931a
Gabriel Caliari <gabriel15caliari@hotmail.com>
2024-07-11 11:50:51 -0300

    inserindo o button no components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,31 @@
- import { Input } from "@/components/input";
+import { Input } from "@/components/input";
 import { Image, Text, View} from "react-native";
+import {MapPin, Calendar as IconCalendar} from "lucide-react-native";
+import { colors } from "@/styles/colors";
+import { Button } from "@/components/button";
+
  export default function Index(){
     return (
-      <View className="flex-1 items-center justify-center">
+      <View className="flex-1 items-center justify-center px-5">
         <Image source={require("@/assets/logo.png")} className="h-8" resizeMode="contain"/>
         <Text className="text-zinc-400 font-regular text-center text-lg mt-3">
           Convide seus amigos e planeje sua{"\n"}próxima viagem
         </Text>
-        <View>
-          <Input variant="primary">
-            <Input.Field />
+        <View className="w-full bg-zinc-900 p-4 rounded-xl my-8 border border-zinc-800 ">
+          <Input>
+            <MapPin color={colors.zinc[400]} size={20}/>
+            <Input.Field placeholder="Para onde?"/>
           </Input>
+          <Input>
+            <IconCalendar color={colors.zinc[400]} size={20}/>
+            <Input.Field placeholder="Para onde?"/>
+          </Input>
+
+          <View className="border-b py-3 border-zinc-800" >
+              <Button variant="primary">
+                <Button.Title>Alterar local/data</Button.Title>
+              </Button>
+          </View>
         </View>
       </View>
     )
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { TextInput, TextInputProps, View } from "react-native";
+import { Platform, TextInput, TextInputProps, View } from "react-native";
 import clsx from 'clsx'
 import { colors } from "@/styles/colors";
 
@@ -14,9 +14,9 @@ function Input({children, variant = 'primary'}: InputProps) {
   return (
     <View className={clsx(
     "w-full h-16 flex-row items-center gap-2",
-    {"h-14 px-4 rounded-lg border-zinc-800": variant !== "primary"},
-    {"bg-zinc-950": variant === "secondary"},
-    {"bg-zinc-900": variant === "tertiary"}
+    { "h-14 px-4 rounded-lg border-zinc-800": variant !== "primary",
+      "bg-zinc-950": variant === "secondary",
+      "bg-zinc-900": variant === "tertiary"}
   )}>
       {children}
     </View>
@@ -29,6 +29,7 @@ function Field({...rest}: TextInputProps) {
   className="flex-1 text-zinc-100 text-lg font-regular"
   placeholderTextColor={colors.zinc[400]}
   cursorColor={colors.zinc[100]}
+  selectionColor={Platform.OS === 'ios' ? colors.zinc[100] : undefined}
   {...rest}
   />
   )
