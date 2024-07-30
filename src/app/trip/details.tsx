c0cbfe76685ab2f727715da56f76bcbe43f4bfb6
Gabriel Caliari <gabriel15caliari@hotmail.com>
2024-07-30 14:14:23 -0300

    inserindo os convidados na aba de detalhes e mostrando se ele estao confirmados ou nao

--- src/app/trip/details.tsx
+++ src/app/trip/details.tsx
@@ -1,12 +1,15 @@
 import { Button } from "@/components/button";
 import { Input } from "@/components/input";
 import { Modal } from "@/components/modal";
+import { Participant, ParticipantProps } from "@/components/participant";
+import { TripLink, TripLinkProps } from "@/components/tripLink";
 import { linksServer } from "@/server/links-server";
+import { participantsServer } from "@/server/participants-server";
 import { colors } from "@/styles/colors";
 import { validateInput } from "@/utils/validateInput";
 import { Plus } from "lucide-react-native";
-import React, { useState } from "react";
-import { Alert, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Alert, FlatList, Text, View } from "react-native";
 import { set } from "zod";
 
 export const  Details = ({tripId}: {tripId: string}) =>  {
@@ -14,6 +17,8 @@ export const  Details = ({tripId}: {tripId: string}) =>  {
   const [linkName, setLinkName] = useState("")
   const [linkURL, setLinkURL] = useState("")
   const [isCreatingLinkTrip, setIsCreatingLinkTrip] = useState(false)
+  const [links, setLinks] = useState<TripLinkProps[]>([])
+  const [participants, setParticipants] = useState<ParticipantProps[]>([])
 
   function resetNewLinkFields(){
     setLinkName("")
@@ -27,8 +32,8 @@ export const  Details = ({tripId}: {tripId: string}) =>  {
         return Alert.alert("Link", "Informe um titulo para o link");
       }
 
-      if(linkName.trim()){
-        return Alert.alert("Link", "Informe um titulo para o link");
+      if(!validateInput.url(linkURL.trim())){
+        return Alert.alert("Link", "Link invalido!");
       }
 
       setIsCreatingLinkTrip(true)
@@ -41,6 +46,7 @@ export const  Details = ({tripId}: {tripId: string}) =>  {
 
       Alert.alert("Link", "Link criado com sucesso!")
       resetNewLinkFields()
+      await getTripLinks()
     } catch (error) {
 
     } finally {
@@ -48,6 +54,28 @@ export const  Details = ({tripId}: {tripId: string}) =>  {
     }
   }
 
+  async function getTripLinks() {
+      try {
+        const links = await linksServer.getLinksByTripId(tripId)
+        setLinks(links)
+      } catch (error) {
+
+      }
+  }
+
+  async function getTripParticipants() {
+    try {
+      const participants = await participantsServer.getByTripId(tripId)
+      setParticipants(participants)
+    } catch (error) {
+
+    }
+  }
+
+  useEffect(() => {
+    getTripLinks()
+    getTripParticipants()
+  }, [])
 
   return (
   <View className="flex-1 mt-10">
@@ -56,10 +84,33 @@ export const  Details = ({tripId}: {tripId: string}) =>  {
     </Text>
 
   <View className="flex-1">
+    {links.length > 0 ?  <FlatList
+     data={links}
+     keyExtractor={(item) => item.id}
+     renderItem={({ item }) => <TripLink data={item} />}
+     contentContainerClassName="gap-4"
+    /> : (
+    <Text className="text-zinc-400 font-regular text-base mt-2 mb-6">
+      Nenhum link adicionado.
+    </Text>)}
+
     <Button variant="secondary" onPress={() => setShowNewLinkModal(true)}>
       <Plus color={colors.zinc[200]} size={20}/>
       <Button.Title>Cadastrar novo link</Button.Title>
     </Button>
+  </View>
+
+  <View className="flex-1 border-t border-zinc-800 mt-6">
+   <Text className="text-zinc-50 text-2xl font-semibold my-6">
+      Convidados
+    </Text>
+
+    <FlatList
+      data={participants}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item}) => <Participant data={item}/>}
+      contentContainerClassName="gap-4"
+    />
   </View>
 
   <Modal
